7bf64616fb3cb85cf90b04a22d0d912ac51d9410
yash-neural <ypmohod.go@gmail.com>
2026-05-06 19:38:59 +0530

    Day 3

--- Day 1 Logging in python/main.py
+++ Day 1 Logging in python/main.py
@@ -1,1 +1,6 @@
 print("Welcome to python")
+
+
+print("Hello world \ntest")
+
+print("Welcome to python" + " new course")
---
+++ Day 2 Data Types /main.py
@@ -0,0 +1,78 @@
+# () => ** , // => *, / => + , -
+
+# print((2*2) / 3 + 1 - (1 + 2))
+#           4   // 3  +  1 - 3
+#              1 + 1 - 3
+#
+# print(4 // 3)
+
+
+
+# a = 100
+# b = 200
+
+# a += b
+# a -= b
+# a *= b
+# a /=b
+# print(a)
+
+
+
+
+
+# Unary Operators: Operate on a single operand.
+
+# number = 10
+# result = -number
+# print(result)  # -10
+
+# number = 10
+# result = +number
+# print(result)   # 10
+
+# is_true = True
+#
+# print(not is_true)   #False
+
+
+# Binary addition (+) operator
+# a = 5
+# b = 3
+# addition_result = a + b
+# print("Binary Addition Operator:", addition_result)
+#
+# # Binary multiplication (*) operator
+# multiplication_result = a * b
+# print("Binary Multiplication Operator:", multiplication_result)
+#
+# # Logical AND (&) operator
+# bitwise_and_result = a & b
+# print("Bitwise AND Operator:", bitwise_and_result)
+#
+# bitwise_or_result = 0 | b
+# print("Bitwise OR Operator:", bitwise_or_result)
+
+
+#
+# result = 5 + 3 * 2 / 2 - (4 % 3) ** 2
+#
+#
+# print("Result:", result)
+
+weight = 85
+height = 1.65
+
+bmi = weight / height ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi, 2))
+
+
+
+
+
+
+
